dffd3e9a3f9455eb83fa181224c2612230109c2b
turliakov <123>
2020-05-22 09:57:10 +0400

    one more test of path

--- src/main/java/Regular.java
+++ src/main/java/Regular.java
@@ -1,37 +1,41 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/test")
 public class Regular {
 
     @GET
-    @Path("test1/{id: [0-9]*}")
+    @Path("test1/{id:[0-9]*}")
     @Produces(MediaType.TEXT_PLAIN)
     public String test1(@PathParam("id") Long id) {
-        return "id: " + id.toString();
+        return "id: " + id;
     }
 
     @GET
     @Path("test2/{id: \\d+?}")
     @Produces(MediaType.TEXT_PLAIN)
     public String test2(@PathParam("id") Long id) {
-        return "id: " + id.toString();
+        return "id: " + id;
     }
 
     @GET
     @Path("{id : [0-9]+?}")
     @Produces(MediaType.TEXT_PLAIN)
     public String test3(@PathParam("id") Long id) {
-        return "id: " + id.toString();
+        return "id: " + id;
     }
 
     @GET
     @Path("{id: [0-9]*?}")
     @Produces(MediaType.TEXT_PLAIN)
     public String test4(@PathParam("id") Long id) {
-        return "id: " + id.toString();
+        return "id: " + id;
+    }
+
+    @GET
+    @Path("/log/top{p:/?}{numberOfItems:([0-9]*)}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test5(@DefaultValue("null") @PathParam(value = "numberOfItems") Integer numberOfItems) {
+        return "id: " + numberOfItems;
     }
 }
